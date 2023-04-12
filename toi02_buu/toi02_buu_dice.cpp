7e0f30eb43536db6d3652f91e946787edf37ef3a
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-12 14:49:55 +0700

    Add files via upload

---
+++ toi02_buu/toi02_buu_dice.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    cin >> N;
+    string R;
+    vector<int> dice(N,1);
+    for (int i=0; i<N; i++){
+        cin >> R;
+    }
+}
---
+++ toi02_buu/toi02_buu_skyline.cpp
@@ -0,0 +1,34 @@
+/*
+CONTEST: 2nd POSN - Olympiad in Informatics
+HOST: Burapha University, Chonburi
+TASK: Skyline (toi2_skyline & 1008)
+LANG: C++
+AUTHOR: Mr.Thanpisit Naowapradit
+CENTER: Prince Songkla University, Pattani Campus
+SCHOOL: Phimanpittayasan School, Satun
+WRITTEN ON: 12/04/2023
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N,L,H,R;
+    cin >> N;
+    vector<int> skyline(3001,0);
+    for (int i=0; i<N; i++){
+        cin >> L >> H >> R;
+        for (int j=L; j<R; j++){
+            skyline[j] = max(skyline[j],H);
+        }
+    }
+    int current = 0;
+    for (int i=1; i<=3000; i++){
+        if (skyline[i]!=current){
+            cout << i << " " << skyline[i] << " ";
+            current = skyline[i];
+        }
+    }
+    return 0;
+}
