87043ca5fdb902bf72f7f6b361511af73f0f91a2
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-12 14:45:56 +0700

    Update toi01_ku_roman.cpp

--- toi01_ku/toi01_ku_roman.cpp
+++ toi01_ku/toi01_ku_roman.cpp
@@ -3,7 +3,7 @@ TASK: Roman (toi1_roman & 1002)
 TASK_SOURCE: https://beta.programming.in.th/tasks/toi1_roman
              https://beta.programming.in.th/tasks/1002
 LANG: C++
-AUTHOR: EthanWinter
+AUTHOR: EthanWinter (Thanpisit Naowapradit)
 WRITTEN ON: 11/04/2023
 */
 
