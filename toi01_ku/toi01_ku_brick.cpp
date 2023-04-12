ee056b8eb67c59124ee738cbcbe46ff9504e4d8a
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-12 20:44:54 +0700

    Update toi01_ku_brick.cpp

--- toi01_ku/toi01_ku_brick.cpp
+++ toi01_ku/toi01_ku_brick.cpp
@@ -4,6 +4,7 @@ TASK_SOURCE: https://beta.programming.in.th/tasks/toi1_brick
              https://beta.programming.in.th/tasks/1001
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
+CENTER: Prince Songkla University, Pattani Campus (PTN)
 WRITTEN ON: 11/04/2023
 */
 
