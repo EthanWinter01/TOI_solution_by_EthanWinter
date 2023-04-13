c6c9824e4c49c8f3bf51ef770d4a9ceb7c38cce6
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-13 14:22:03 +0700

    Update toi01_ku_chain.cpp

--- toi01_ku/toi01_ku_chain.cpp
+++ toi01_ku/toi01_ku_chain.cpp
@@ -1,6 +1,4 @@
 /*
-CONTEST: 1st POSN - Olympiad in Informatics, 2005
-HOST: Kasetsart University, Bangkok
 TASK: Word Chain (toi1_chain & 1000)
 SOURCE: 
     https://beta.programming.in.th/tasks/toi1_chain
@@ -8,7 +6,6 @@ SOURCE:
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
 CENTER: Prince Songkla University, Pattani Campus (PTN)
-SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 11/04/2023
 */
 
