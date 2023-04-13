b4a27d052b7a9eefc0dceb9cfce16adc9a620419
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-13 16:14:01 +0700

    Update toi03_kku_treasure.cpp

--- toi03_kku/toi03_kku_treasure.cpp
+++ toi03_kku/toi03_kku_treasure.cpp
@@ -1,6 +1,4 @@
 /*
-CONTEST: 3rd POSN - Olympiad in Informatics, 2007
-HOST: Khon Khaen University, Khon Khaen
 TASK: Treasure (toi3_treasure & 1016)
 SOURCE:
     https://beta.programming.in.th/tasks/toi3_treasure
@@ -8,7 +6,6 @@ SOURCE:
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
 CENTER: Prince Songkla University, Pattani Campus (PTN)
-SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 13/04/2023
 */
 
