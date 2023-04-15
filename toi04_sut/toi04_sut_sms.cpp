4b8a11fd18434fada692470988c511b983f80311
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-15 17:42:41 +0700

    Update toi04_sut_sms.cpp

--- toi04_sut/toi04_sut_sms.cpp
+++ toi04_sut/toi04_sut_sms.cpp
@@ -1,6 +1,4 @@
 /*
-CONTEST: 4th POSN - Olympiad in Informatics, 2008
-HOST: Suranaree University of Technology, Nakhonratchasrima
 TASK: SMS Thumb (toi4_sms & 1059)
 SOURCE:
     https://beta.programming.in.th/tasks/toi4_sms
@@ -8,7 +6,6 @@ SOURCE:
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
 CENTER: Prince Songkla University, Pattani Campus
-SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 15/04/2023
 */
 
