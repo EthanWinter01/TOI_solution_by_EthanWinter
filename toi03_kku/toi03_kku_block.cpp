c63c00f33083985ddb5db192a0d230913dc889bd
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-17 20:27:52 +0700

    Add files via upload

---
+++ toi03_kku/toi03_kku_block.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define pii pair<int,int>
+using namespace std;
+int m,n,l,score=0;
+vector<int> dir = {0,1,0,-1,0};
+vector<vector<char>> grid(5,vector<char>(5));
+bool isCheated(int r, int c, char op){
+    int k = 0;
+    (op == 'L')? k-- : k++;
+    if (grid[r][c]=='#' || grid[r][c]=='-' || grid[r][c+k]!='-')
+        return true;
+    return false;
+}
+void dropCheck(){
+    for (int j=0; j<n; j++){
+        for (int i=m-1; i>0; i--){
+            if (grid[i][j] == '-' && (grid[i-1][j]!='#'&&grid[i-1][j]!='-')){
+                swap(grid[i][j],grid[i-1][j]);
+                i=m-1;
+            }
+        }
+    }
+}
+void dfs(int r, int c, char check){
+    stack<pii> S;
+    vector<vector<bool>> visited(5,vector<bool>(5,false));
+    S.push({r,c});
+    visited[r][c] = true;
+    bool scoring = false;
+    while (!S.empty()){
+        int row = S.top().f;
+        int col = S.top().s;
+        S.pop();
+        for (int i=0; i<4; i++){
+            int newRow = row+dir[i];
+            int newCol = col+dir[i+1];
+            if (newRow>=0 && newCol>=0 && newRow<m && newCol<n && !visited[newRow][newCol] && grid[newRow][newCol]==check){
+                scoring = true;
+                visited[newRow][newCol] = true;
+                S.push({newRow,newCol});
+            }
+        }
+    }
+    if (scoring){
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (visited[i][j]){
+                    grid[i][j] = '-';
+                    score += 5;
+                }
+            }
+        }
+    }
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> m >> n;
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            cin >> grid[i][j];
+    cin >> l;
+    while (l--){
+        int r,c;
+        char op;
+        cin >> r >> c >> op;
+        if (isCheated(r,c,op)){
+            score -= 5;
+            continue;
+        }
+        if (op == 'L')
+            swap(grid[r][c],grid[r][c-1]);
+        else
+            swap(grid[r][c],grid[r][c+1]);
+        dropCheck();
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+                if (grid[i][j]!='#' && grid[i][j]!='-')
+                    dfs(i,j,grid[i][j]);
+        dropCheck();
+    }
+    cout << score << "\n";
+    for (int i=0; i<m; i++){
+        for (int j=0; j<n; j++)
+            cout << grid[i][j] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
