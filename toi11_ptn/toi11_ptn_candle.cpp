6f9516bddf04d1a9840e259cb53ae9894af2c965
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-17 23:25:43 +0700

    Update toi11_ptn_candle.cpp

--- toi11_ptn/toi11_ptn_candle.cpp
+++ toi11_ptn/toi11_ptn_candle.cpp
@@ -1,10 +1,13 @@
 /*
+CONTEST: 11th Thailand Olympiad in Informatics, 2015
+HOST: Prince Songkla University, Pattani Campus, Trang
 TASK: Candle Light Prayer (toi11_candle)
 TASK_SOURCE:
     https://beta.programming.in.th/tasks/toi11_candle
 LANG: C++
 AUTHOR: Mr.Thanpisit Naowapradit
 CENTER: Prince Songkla University, Pattani Campus
+SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 12/04/2023
 */
 
