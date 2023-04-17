9be658f8aef23abffcfbcc5de173a298f4869fb4
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-17 22:47:15 +0700

    Update toi01_ku_brick.cpp

--- toi01_ku/toi01_ku_brick.cpp
+++ toi01_ku/toi01_ku_brick.cpp
@@ -1,4 +1,6 @@
 /*
+CONTEST: 1st POSN - Olympiad in Informatics, 2005
+HOST: Kasetsart University, Bangkok
 TASK: Brick (toi1_brick & 1001)
 SOURCE: 
     https://beta.programming.in.th/tasks/toi1_brick
@@ -6,6 +8,7 @@ SOURCE:
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
 CENTER: Prince Songkla University, Pattani Campus (PTN)
+SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 11/04/2023
 */
 
