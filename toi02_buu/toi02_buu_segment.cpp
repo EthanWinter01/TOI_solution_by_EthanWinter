5f788ade29dbb1ab3490d06f31e5962ef43b5a39
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-04-17 16:46:38 +0700

    Update toi02_buu_segment.cpp

--- toi02_buu/toi02_buu_segment.cpp
+++ toi02_buu/toi02_buu_segment.cpp
@@ -1,6 +1,4 @@
 /*
-CONTEST: 2nd POSN - Olympiad in Informatics, 2006
-HOST: Burapha University, Chonburi
 TASK: Segment (toi2_segment & 1009)
 SOURCE:
     https://beta.programming.in.th/tasks/toi2_segment
@@ -8,7 +6,6 @@ SOURCE:
 LANG: C++
 AUTHOR: EthanWinter (Thanpisit Naowapradit)
 CENTER: Prince Songkla University, Pattani Campus (PTN)
-SCHOOL: Phimanpittayasan School, Satun
 WRITTEN ON: 17/04/2023
 */
 
