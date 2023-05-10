392d943ddd0fbffd872fa96472ef63634cb19627
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-05-10 17:03:00 +0700

    Update toi08_electricity.cpp

--- toi08_su/toi08_su_electricity.cpp
+++ toi08_su/toi08_su_electricity.cpp
@@ -1,3 +1,17 @@
+/*
+CONTEST: 8th Thailand Olympiad in Informatics, 2012
+HOST: Silapakorn University, Bangkok
+TASK: Electricity (toi8_electricity & 1157)
+SOURCE:
+    https://beta.programming.in.th/tasks/toi8_electricity
+    https://beta.programming.in.th/tasks/1157
+LANG: C++
+AUTHOR: EthanWinter (Thanpisit Naowapradit)
+CENTER: Prince Songkla University, Pattani Campus
+SCHOOL: Phimanpittayasan School, Satun
+WRITTEN ON: 10/05/2023
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main(void){
@@ -5,7 +19,23 @@ int main(void){
     cin.tie(NULL);
     int N,k;
     cin >> N >> k;
-    vector<int> P(N+1);
-    for (int i=1; i<=N; i++)
-        cin >> P[i];
+    deque<pair<int,int>> DQ;
+    for (int i=1; i<=N; ++i){
+        int p;
+        cin >> p;
+        if (i == 1){
+            DQ.push_back({p,1});
+            continue;
+        }
+        if (i == N){
+            cout << DQ.front().first+p;
+            return 0;
+        }
+        int temp = DQ.front().first+p;
+        while (!DQ.empty() && i-DQ.front().second >= k)
+            DQ.pop_front();
+        while (!DQ.empty() && DQ.back().first > temp)
+            DQ.pop_back();
+        DQ.push_back({temp,i});
+    }
 }
