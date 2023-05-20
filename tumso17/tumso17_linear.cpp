54024dc5e72175fd71f6eb55a0f2b02b4d006079
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-05-20 23:35:53 +0700

    Add files via upload

---
+++ tumso17/tumso17_linear.cpp
@@ -0,0 +1,22 @@
+/*
+CONTEST: 17th Triam Udom Mathematics and Science Olympiad, 2020
+HOST: Triam Udom Suksa School
+TASK: สมการเชิงเส้นตัวแปรเดียว (tumso17_linear)
+SOURCE:
+    https://beta.programming.in.th/tasks/tumso17_linear
+LANG: C++
+AUTHOR: EthanWinter (Thanpisit Naowapradit)
+SCHOOL: Phimanpittayasan School
+WRITTEN ON: 20/05/2023
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long A,B;
+    cin >> A >> B;
+    cout << B/A;
+    return 0;
+}
