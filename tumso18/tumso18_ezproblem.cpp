dd94104de4086c5a03b0aa1982a60e3e95fa740b
EthanWinter01 <108206007+EthanWinter01@users.noreply.github.com>
2023-05-20 23:53:30 +0700

    Update tumso18_ezproblem.cpp

--- tumso18/tumso18_ezproblem.cpp
+++ tumso18/tumso18_ezproblem.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-
+ll mod = 1e9+7;
+ll bitPower(ll x, ll n){
+    x %= mod;
+    ll result = 1;
+    while (n){
+        if (n%2)
+            result = (result*x)%mod;
+        x = (x*x)%mod;
+        n /= 2;
+    }
+    return result;
+}
 int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int t;
+    cin >> t;
+    while (t--){
+        ll n,a;
+        cin >> n >> a;
+        ll fm = bitPower(a*2-1,mod-2);
+        ll answer = (bitPower(a*2,n)-1)*bitPower(a*2,n-1)%mod;
+        answer = (answer*fm)%mod;
+        cout << answer << "\n";
+    }
+    return 0;
 }
